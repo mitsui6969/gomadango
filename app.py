9cd17dfa7277c08cd9671d4daddb4f8f6c2d94cb
nagisa <c1318728@st.kanazawa-it.ac.jp>
2023-08-07 11:17:24 +0900

    ログインと新規登録の認証&失敗時のコメント追加

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import pytz
 
 app = Flask(__name__)
@@ -32,43 +33,61 @@ class List(UserMixin, db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+###アカウント新規作成###
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        stu_num = request.form['stu_num']
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(stu_num=stu_num).first()
-        if user is None:
-            user = User(stu_num=stu_num, password=password, email=email)
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('index'))
+        stu_num = request.form.get('stu_num')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+
+        if user: # メールがすでに登録されてたらコメント出す処理
+            flash('このメールアドレスはすでに登録されています')
+            return redirect(url_for('register'))
+        
+        new_user = User(stu_num=stu_num, password=password, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('index'))
+        # else: # if a user is found, we want to redirect back to signup page so user can try again
+        #     flash('Email address already exists')
+        #     return redirect(url_for('register'))
     return render_template('register.html')
 
+###ログイン画面###
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
         user = User.query.filter_by(email=email, password=password).first()
-        if user:
+        ##メールとパスが一致しなかったらコメント出す処理
+        if not user or not User.query.filter_by(email=email).first():
+            flash('ログイン情報が一致しません。もう一度お試しください')
+            return redirect(url_for('login'))
+        ##一致したらホームに飛ばす処理
+        elif user:
             login_user(user)
             return redirect(url_for('index'))
     return render_template('login.html')
 
+###ログアウト###
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+###ホーム###
 @app.route('/')
 def index():
     lists = List.query.all()
     return render_template('index.html', lists = lists)
 
+###予約画面###
 @app.route('/new', methods=['GET', 'POST'])
 @login_required
 def new():
